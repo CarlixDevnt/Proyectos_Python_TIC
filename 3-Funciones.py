cdc61a195a7f546e10f4b69b60a20355f391e799
CarlixDevn't <88063767+CarlixDevnt@users.noreply.github.com>
2022-06-14 12:35:23 +0200

    Update and rename 3-Funciones (UNFINISHED).py to 3-Funciones.py

--- 3-Funciones.py
+++ 3-Funciones.py
@@ -34,8 +34,45 @@ print("----------------") # Una vez más, la importancia de las tabulaciones par
 
 # Puedes usar los bucles para repetir un proceso en un rango (función range()) de veces
 for i in range(1, 10):
-    print("Hola, soy un programa acosador")
+    print("Hola, soy un programa acosador que pondrá este mensaje 10 veces")
 
+# Otra función interesante es la función condicional if(condition), que ejecuta un código si verifica una condición especificada dentro del paréntesis
+# if se complementa con else, la cual ejecutará un código cuando no se verifique la condición del if original
+# Ejemplo de if suelto
+if 2 + 3 == 5:
+    print("Sí, 2 + 3 = 5")
 
+# Ejemplo de estructura if() else
+if 2 > 3:
+    print("2 es mayor que 3")
+else:
+    print("2 no es mayor que 3")
 
+# Las condiciones pueden ser de varios tipos
+# - De presencia o verificación cuando se usa un suceso suelto
+isIn = true
+if isIn:
+    print("Está dentro")
+else:
+    print("Está fuera")
+# - De ausencia o falsedad cuando se usa un suceso precedido de not
+checking = false
+if not checking:
+    print("No se ha realizado la revisión")
+else:
+    print("Se ha realizado la revisión")
+# - De comparativa entre dos elementos, la cual puede ser de superioridad (>), inferioridad (<), igualdad (=) o igualdad específica (==)
+
+# Como en probabilidad, aquí también se pueden juntar varias condiciones en una única función.
+# - and para condiciones conjuntas, se tienen que cumplir ambas condiciones para que se ejecute el código
+if 2+3==5 and isIn:
+    print("2+3 son 5 y sí, está dentro")
+else:
+    print("O bien 2+3 no son 5 o no está dentro")
+# - or para condicionales
+if 2+3==5 or isIn:
+    print("O 2+3 son 5 o está dentro")
+else:
+    print("Ni 2+3 son 5 ni está dentro")
+    
 # Hecho por Carlos, alumno de TIC de 2º de Bachillerato en el IES Auga da Laxe
