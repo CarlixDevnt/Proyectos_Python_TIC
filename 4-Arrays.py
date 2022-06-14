84383003c405d4d2e94f6009f0c9879c9e9a8405
CarlixDevn't <88063767+CarlixDevnt@users.noreply.github.com>
2022-06-14 12:38:55 +0200

    Create 4-Arrays.py

---
+++ 4-Arrays.py
@@ -0,0 +1,3 @@
+# Aquí aprenderás a hacer listas de la compra
+
+# Ya hemos hablado de los Arrays a lo largo de las 3 prácticas anteriores, y ya deberías saber lo que son. Aquí aprenderás a utilizarlos y a hacer cosas interesantes con ellos
